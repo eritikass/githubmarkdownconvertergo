b20b975b0304b5fe2c0b45251867651220b26a2b
Siim Tiilen <eritikass@gmail.com>
2020-02-07 00:10:00 +0200

    add github to slack parsing

--- slack.go
+++ slack.go
@@ -1,6 +1,59 @@
 package githubmarkdownconvertergo
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 // Slack returns github markdown converted to slack
 func Slack(markdown string) string {
+	var re *regexp.Regexp
+
+	linkRegex := ".*"
+
+	// bold **TEXT**  -> *TEXT*
+	re = regexp.MustCompile(`(?miU)((\*\*).+(\*\*))`)
+	markdown = re.ReplaceAllStringFunc(markdown, func(s string) string {
+		return strings.ReplaceAll(s, "**", "*")
+	})
+
+	// italic same for both _TEXT_
+
+	// strikethrough ~~TEXT~~  -> ~TEXT~
+	re = regexp.MustCompile(`(?miU)((~~).+(~~))`)
+	markdown = re.ReplaceAllStringFunc(markdown, func(s string) string {
+		return strings.ReplaceAll(s, "~~", "~")
+	})
+
+	// links [TEXT](link) -> <link|TEXT>
+	re = regexp.MustCompile(`(?miU)(\[(?P<name>.+)\]\((?P<link>` + linkRegex + `)\))`)
+	markdown = re.ReplaceAllStringFunc(markdown, func(s string) string {
+		match := re.FindStringSubmatch(s)
+		name := ""
+		link := ""
+		for i, n := range re.SubexpNames() {
+			if i != 0 && n == "name" {
+				name = match[i]
+			}
+			if i != 0 && n == "link" {
+				link = match[i]
+			}
+		}
+		if name != "" && link != "" {
+			return fmt.Sprintf("<%s|%s>", link, name)
+		}
+		return s
+	})
+
+	// * -> •
+	re = regexp.MustCompile(`(?s)([^\n][ ]{1,}\*)`)
+	markdown = re.ReplaceAllStringFunc(markdown, func(s string) string {
+		re2 := regexp.MustCompile(`^([ ]+)?(\*)`)
+		return re2.ReplaceAllString(s, "$1•")
+	})
+	re = regexp.MustCompile(`(?m)((\n[ ]{0,})(\*))`)
+	markdown = re.ReplaceAllString(markdown, "$2•")
+
 	return markdown
 }
--- slack_test.go
+++ slack_test.go
@@ -9,5 +9,42 @@ import (
 func TestSlack(t *testing.T) {
 	assert := assert.New(t)
 
-	assert.Equal(Slack("a"), "a")
+	assert.Equal("a", Slack("a"))
+	assert.Equal("text *bold* more-text", Slack("text **bold** more-text"))
+	assert.Equal("text ~strike~ more-text", Slack("text ~~strike~~ more-text"))
+	// mix of strike + bold
+	assert.Equal("text ~strike~ more*bold*-te*x*t", Slack("text ~~strike~~ more**bold**-te**x**t"))
+	// links
+	assert.Equal("text <http://www.foo.com|This is link title> more-text", Slack("text [This is link title](http://www.foo.com) more-text"))
+	// two links and bold
+	assert.Equal("text <http://google.com/|Google> (<https://xxx.com/|x*BB*x>) more-text", Slack("text [Google](http://google.com/) ([x**BB**x](https://xxx.com/)) more-text"))
+	// list
+	listGithub := `   *
+    aaa
+				* *
+    bbb  * cccc
+*
+    ddd
+      *`
+	listSlack := `   •
+    aaa
+				• *
+    bbb  * cccc
+•
+    ddd
+      •`
+
+	assert.Equal(listSlack, Slack(listGithub))
+
+	// release message
+	msgGithub := `# [1.50.0](https://github.com/foo/boo/compare/v1.49.3...v1.50.0) (2015-02-12)
+### Features
+ * add GET /v1/events ([#134](https://github.com/foo/boo/issues/134)) ([1726806](https://github.com/foo/boo/commit/1726806))
+ * remove DELETE /v1/message ([#121](https://github.com/foo/boo/issues/121)) ([3523r42](https://github.com/foo/boo/commit/3523r42))`
+	msgSlack := `# <https://github.com/foo/boo/compare/v1.49.3...v1.50.0|1.50.0> (2015-02-12)
+### Features
+ • add GET /v1/events (<https://github.com/foo/boo/issues/134|#134>) (<https://github.com/foo/boo/commit/1726806|1726806>)
+ • remove DELETE /v1/message (<https://github.com/foo/boo/issues/121|#121>) (<https://github.com/foo/boo/commit/3523r42|3523r42>)`
+
+	assert.Equal(msgSlack, Slack(msgGithub))
 }
